da07c18a65482dca20178e63ab578b03025faf6a
wiola <wiolac0032@wp.pl>
2019-11-16 11:27:13 +0100

    [add] controller more endpoints

--- src/main/java/com/example/myspringexample/controller/HelloController.java
+++ src/main/java/com/example/myspringexample/controller/HelloController.java
@@ -20,4 +20,17 @@ public class HelloController {
         modelAndView.setViewName("home");
         return modelAndView;
     }
+
+    @GetMapping("/hello2")
+    ModelAndView mySecondView(ModelAndView modelAndView, @RequestParam(defaultValue = "Franek") String name, @RequestParam(defaultValue = "Nowak") String surname, @RequestParam(defaultValue = "20") int age) {
+        logger.info("name: {}", name);
+
+
+        modelAndView.addObject("name", name);
+        modelAndView.addObject("surname", surname);
+        modelAndView.addObject("age", age);
+        modelAndView.setViewName("home2");
+        return modelAndView;
+    }
+
 }
