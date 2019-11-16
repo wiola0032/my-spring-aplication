db47694f932d3d2d91804734d99fa27d50d288f3
wiola <wiolac0032@wp.pl>
2019-11-16 13:17:49 +0100

    [add] - some info

--- src/main/java/com/example/myspringexample/MySpringExampleApplication.java
+++ src/main/java/com/example/myspringexample/MySpringExampleApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MySpringExampleApplication {
 
+    // this method runs Spring container
     public static void main(String[] args) {
         SpringApplication.run(MySpringExampleApplication.class, args);
     }
